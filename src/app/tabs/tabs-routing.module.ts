4c554755b3de3d202a36b5d8cdf86cb719083c21
Deku000 <jacksonalipao.ja@gmail.com>
2021-09-26 09:26:29 +0800

    renamed

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
     children: [
       {
         path: 'tab1',
-        loadChildren: () => import('../Image/tab1.module').then(m => m.Tab1PageModule)
+        loadChildren: () => import('../Login/tab1.module').then(m => m.Tab1PageModule)
       },
       {
         path: 'tab2',
